a40f776fad5d9cb5c0f21667e9f798b858a3bba1
Gana Teja Akula <ganateja.akula1998@mail.com>
2023-10-10 05:40:17 -0400

    build fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:baaef99f4d8cfb7a51917bfff9407d41687d5b29a95462babe2c17dca5c3bdf9
-size 435
+oid sha256:449e0d7bc61353f00e7a189ccc72ac4b88b8a3908d01ec19879bd6a4b8109421
+size 395
