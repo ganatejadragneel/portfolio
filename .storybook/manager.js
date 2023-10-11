b60d03307ecc48d52bb85247c446d411f497266c
Gana Teja Akula <ganateja.akula1998@mail.com>
2023-10-11 00:28:21 -0400

    nit

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d541205697aae63ed103b9dce01977cc21023d9bca0f6f653b9143480b1bd0ec
-size 280
+import { themes } from '@storybook/theming';
+import { addons } from '@storybook/addons';
+
+addons.setConfig({
+  theme: {
+    ...themes.dark,
+    brandImage: 'https://hamishw.com/icon.svg',
+    brandTitle: 'Gana Teja Akula Components',
+    brandUrl: 'https://hamishw.com',
+  },
+});
